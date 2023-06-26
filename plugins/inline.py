280b0f394e3237b0938a6805bff831098bb38748
Vishu-993 <116909259+Vishu-993@users.noreply.github.com>
2023-06-27 00:14:53 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -3,21 +3,16 @@ from pyrogram import Client, emoji, filters
 from pyrogram.errors.exceptions.bad_request_400 import QueryIdInvalid
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultCachedDocument, InlineQuery
 from database.ia_filterdb import get_search_results
-from utils import is_subscribed, get_size, temp
-from info import CACHE_TIME, AUTH_USERS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION
+from utils import get_size, temp
+from info import CACHE_TIME, CUSTOM_FILE_CAPTION
 
 logger = logging.getLogger(__name__)
-cache_time = 0 if AUTH_USERS or AUTH_CHANNEL else CACHE_TIME
+cache_time = CACHE_TIME
 
 async def inline_users(query: InlineQuery):
-    if AUTH_USERS:
-        if query.from_user and query.from_user.id in AUTH_USERS:
-            return True
-        else:
-            return False
-    if query.from_user and query.from_user.id not in temp.BANNED_USERS:
-        return True
-    return False
+    if query.from_user and query.from_user.id in temp.BANNED_USERS:
+        return False
+    return True
 
 @Client.on_inline_query()
 async def answer(bot, query):
@@ -28,13 +23,6 @@ async def answer(bot, query):
                            cache_time=0,
                            switch_pm_text='okDa',
                            switch_pm_parameter="hehe")
-        return
-
-    if AUTH_CHANNEL and not await is_subscribed(bot, query):
-        await query.answer(results=[],
-                           cache_time=0,
-                           switch_pm_text='You have to subscribe my channel to use the bot',
-                           switch_pm_parameter="subscribe")
         return
 
     results = []
@@ -54,15 +42,15 @@ async def answer(bot, query):
                                                   offset=offset)
 
     for file in files:
-        title=file.file_name
-        size=get_size(file.file_size)
-        f_caption=file.caption
+        title = file.file_name
+        size = get_size(file.file_size)
+        f_caption = file.caption
         if CUSTOM_FILE_CAPTION:
             try:
-                f_caption=CUSTOM_FILE_CAPTION.format(file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='' if f_caption is None else f_caption)
+                f_caption = CUSTOM_FILE_CAPTION.format(file_name='' if title is None else title, file_size='' if size is None else size, file_caption='' if f_caption is None else f_caption)
             except Exception as e:
                 logger.exception(e)
-                f_caption=f_caption
+                f_caption = f_caption
         if f_caption is None:
             f_caption = f"{file.file_name}"
         results.append(
@@ -79,11 +67,11 @@ async def answer(bot, query):
             switch_pm_text += f" for {string}"
         try:
             await query.answer(results=results,
-                           is_personal = True,
-                           cache_time=cache_time,
-                           switch_pm_text=switch_pm_text,
-                           switch_pm_parameter="start",
-                           next_offset=str(next_offset))
+                               is_personal=True,
+                               cache_time=cache_time,
+                               switch_pm_text=switch_pm_text,
+                               switch_pm_parameter="start",
+                               next_offset=str(next_offset))
         except QueryIdInvalid:
             pass
         except Exception as e:
@@ -94,19 +82,7 @@ async def answer(bot, query):
             switch_pm_text += f' for "{string}"'
 
         await query.answer(results=[],
-                           is_personal = True,
+                           is_personal=True,
                            cache_time=cache_time,
                            switch_pm_text=switch_pm_text,
-                           switch_pm_parameter="okay")
-
-
-def get_reply_markup(query):
-    buttons = [
-        [
-            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query)
-        ]
-        ]
-    return InlineKeyboardMarkup(buttons)
-
-
-
+                           switch_pm
