2e65ef75d2621df0c1bbad7a87784ec561abf552
Vishu-993 <116909259+Vishu-993@users.noreply.github.com>
2023-07-02 15:02:00 +0530

    Update restart.py

--- plugins/restart.py
+++ plugins/restart.py
@@ -2,6 +2,7 @@ import datetime
 import random
 import time
 import os
+import requests
 
 # Generating a random restart time between 2 and 16 minutes, and 0 to 59 seconds
 restart_time_minutes = random.randint(2, 16)
@@ -9,8 +10,8 @@ restart_time_seconds = random.randint(0, 59)
 restart_time = datetime.timedelta(minutes=restart_time_minutes, seconds=restart_time_seconds)
 
 # Telegram bot setup
-bot_token = os.environ.get("TOKEN", "")
-support_group_id = os.environ.get("GROUP_ID", "")
+bot_token = os.environ.get("TOKEN")
+support_group_id = os.environ.get("GROUP_ID")
 
 # Restart flag
 is_restarted = True
@@ -20,7 +21,14 @@ def send_restart_message():
     restart_message = "⚡ Bot Restarted ⚡\n"
     restart_message += f"🥂 Time Taken: {restart_time_minutes} Minutes {restart_time_seconds} Seconds"
 
-    bot.send_message(chat_id=support_group_id, text=restart_message)
+    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+    data = {
+        "chat_id": support_group_id,
+        "text": restart_message
+    }
+    response = requests.post(url, json=data)
+    if response.status_code != 200:
+        print(f"Failed to send restart message. Error: {response.text}")
 
 # Delay before sending the restart message
 time.sleep(5)  # Adjust the delay as needed
