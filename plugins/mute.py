22722e652fa929f8349ff177d2d662b766dd6ebd
Vishu-993 <116909259+Vishu-993@users.noreply.github.com>
2023-07-03 18:40:29 +0530

    Update mute.py

--- plugins/mute.py
+++ plugins/mute.py
@@ -1,6 +1,6 @@
 import os
-import Update, ParseMode
-import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import Update, ParseMode
 
 # Telegram bot setup
 bot_token = os.environ.get("BOT_TOKEN")
@@ -12,19 +12,22 @@ muted_users = {}
 def mute_user(update: Update, context):
     chat_id = update.effective_chat.id
     user_id = update.message.reply_to_message.from_user.id
+    user_mention = update.message.reply_to_message.from_user.mention_markdown_v2()
 
     # Add the user to the muted_users dictionary
     muted_users[chat_id] = user_id
 
-    update.message.reply_text("User has been muted.")
+    update.message.reply_text(f"User {user_mention} has been muted.")
 
 # Unmute a user
 def unmute_user(update: Update, context):
     chat_id = update.effective_chat.id
+    user_id = update.message.reply_to_message.from_user.id
+    user_mention = update.message.reply_to_message.from_user.mention_markdown_v2()
 
-    if chat_id in muted_users:
+    if chat_id in muted_users and muted_users[chat_id] == user_id:
         del muted_users[chat_id]
-        update.message.reply_text("User has been unmuted.")
+        update.message.reply_text(f"User {user_mention} has been unmuted.")
     else:
         update.message.reply_text("No user is currently muted.")
 
@@ -39,6 +42,8 @@ def handle_message(update: Update, context):
 
     if is_user_muted(chat_id, user_id):
         update.message.delete()
+        user_mention = update.message.from_user.mention_markdown_v2()
+        update.message.reply_text(f"{user_mention}, you are currently muted and cannot send messages.")
 
 # Create the Telegram bot and set up handlers
 updater = Updater(bot_token)
