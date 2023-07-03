15967c496a0fff59f893ce33020f31406a2b20b7
Vishu-993 <116909259+Vishu-993@users.noreply.github.com>
2023-07-03 18:02:33 +0530

    Update restart.py

--- plugins/restart.py
+++ plugins/restart.py
@@ -3,7 +3,7 @@ import random
 import time
 import os
 import requests
-import json
+import InlineKeyboardMarkup, InlineKeyboardButton
 
 # Generating a random restart time between 2 and 16 minutes, and 0 to 59 seconds
 restart_time_minutes = random.randint(2, 16)
@@ -22,20 +22,13 @@ def send_restart_message():
     restart_message = "⚡ Bot Restarted ⚡\n"
     restart_message += f"🥂 Time Taken: {restart_time_minutes} Minutes {restart_time_seconds} Seconds"
 
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("😜 Click Me", callback_data="sticker")]])
+
     url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
     data = {
         "chat_id": support_group_id,
         "text": restart_message,
-        "reply_markup": {
-            "inline_keyboard": [
-                [
-                    {
-                        "text": "😜 Click Me",
-                        "callback_data": "sticker"
-                    }
-                ]
-            ]
-        }
+        "reply_markup": reply_markup.to_json()
     }
     response = requests.post(url, json=data)
     if response.status_code != 200:
@@ -46,11 +39,12 @@ def handle_callback_query(callback_query):
     query_data = callback_query.get("data")
     if query_data == "sticker":
         sticker_url = "https://t.me/addstickers/tyan2d_anim"
-        sticker_message = {
+        reply_message = {
             "chat_id": callback_query["message"]["chat"]["id"],
+            "reply_to_message_id": callback_query["message"]["message_id"],
             "sticker": sticker_url
         }
-        response = requests.post(f"https://api.telegram.org/bot{bot_token}/sendSticker", json=sticker_message)
+        response = requests.post(f"https://api.telegram.org/bot{bot_token}/sendSticker", json=reply_message)
         if response.status_code != 200:
             print(f"Failed to send sticker. Error: {response.text}")
 
@@ -60,13 +54,14 @@ time.sleep(5)  # Adjust the delay as needed
 # Check restart flag and send message
 if is_restarted:
     send_restart_message()
-    
+
 # Handling callback queries (example)
 callback_query_data = {
     "message": {
         "chat": {
             "id": support_group_id
-        }
+        },
+        "message_id": 123456789
     },
     "data": "sticker"
 }
