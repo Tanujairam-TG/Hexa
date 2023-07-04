bc0993ce83d5bb1416cbe665458089810426ac01
Vishu-993 <116909259+Vishu-993@users.noreply.github.com>
2023-07-04 20:20:29 +0530

    Update connection.py

--- plugins/connection.py
+++ plugins/connection.py
@@ -1,166 +1,147 @@
-from typing import Optional, List
+from pyrogram import filters, Client, enums
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from database.connections_mdb import add_connection, all_connections, if_active, delete_connection
+from info import ADMINS
+import logging
 
-from telegram import ParseMode
-from telegram import Message, Chat, Update, Bot, User
-from telegram.error import BadRequest
-from telegram.ext import CommandHandler, Filters
-from telegram.ext.dispatcher import run_async
-from telegram.utils.helpers import mention_html
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.ERROR)
 
-import tg_bot.modules.sql.connection_sql as sql
-from tg_bot import dispatcher, LOGGER, SUDO_USERS
-from tg_bot.modules.helper_funcs.chat_status import bot_admin, user_admin, is_user_admin, can_restrict
-from tg_bot.modules.helper_funcs.extraction import extract_user, extract_user_and_text
-from tg_bot.modules.helper_funcs.string_handling import extract_time
 
-# from tg_bot.modules.translations.strings import tld
+@Client.on_message((filters.private | filters.group) & filters.command('connect'))
+async def addconnection(client, message):
+    userid = message.from_user.id if message.from_user else None
+    if not userid:
+        return await message.reply(f"You are anonymous admin. Use /connect {message.chat.id} in PM")
+    chat_type = message.chat.type
 
-from tg_bot.modules.keyboard import keyboard
+    if chat_type == enums.ChatType.PRIVATE:
+        try:
+            cmd, group_id = message.text.split(" ", 1)
+        except:
+            await message.reply_text(
+                "<b>Enter in correct format!</b>\n\n"
+                "<code>/connect groupid</code>\n\n"
+                "<i>Get your Group id by adding this bot to your group and use  <code>/id</code></i>",
+                quote=True
+            )
+            return
 
-@user_admin
-@run_async
-def allow_connections(bot: Bot, update: Update, args: List[str]) -> str:
-    chat = update.effective_chat  # type: Optional[Chat]
-    if chat.type != chat.PRIVATE:
-        if len(args) >= 1:
-            var = args[0]
-            print(var)
-            if (var == "no"):
-                sql.set_allow_connect_to_chat(chat.id, False)
-                update.effective_message.reply_text("Disabled connections to this chat for users")
-            elif(var == "yes"):
-                sql.set_allow_connect_to_chat(chat.id, True)
-                update.effective_message.reply_text("Enabled connections to this chat for users")
+    elif chat_type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
+        group_id = message.chat.id
+
+    try:
+        st = await client.get_chat_member(group_id, userid)
+        if (
+                st.status != enums.ChatMemberStatus.ADMINISTRATOR
+                and st.status != enums.ChatMemberStatus.OWNER
+                and userid not in ADMINS
+        ):
+            await message.reply_text("You should be an admin in Given group!", quote=True)
+            return
+    except Exception as e:
+        logger.exception(e)
+        await message.reply_text(
+            "Invalid Group ID!\n\nIf correct, Make sure I'm present in your group!!",
+            quote=True,
+        )
+
+        return
+    try:
+        st = await client.get_chat_member(group_id, "me")
+        if st.status == enums.ChatMemberStatus.ADMINISTRATOR:
+            ttl = await client.get_chat(group_id)
+            title = ttl.title
+
+            addcon = await add_connection(str(group_id), str(userid))
+            if addcon:
+                await message.reply_text(
+                    f"Successfully connected to **{title}**\nNow manage your group from my pm !",
+                    quote=True,
+                    parse_mode=enums.ParseMode.MARKDOWN
+                )
+                if chat_type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
+                    await client.send_message(
+                        userid,
+                        f"Connected to **{title}** !",
+                        parse_mode=enums.ParseMode.MARKDOWN
+                    )
             else:
-                update.effective_message.reply_text("Please enter on/yes/off/no in group!")
+                await message.reply_text(
+                    "You're already connected to this chat!",
+                    quote=True
+                )
         else:
-            update.effective_message.reply_text("Please enter on/yes/off/no in group!")
-    else:
-        update.effective_message.reply_text("Please enter on/yes/off/no in group!")
+            await message.reply_text("Add me as an admin in group", quote=True)
+    except Exception as e:
+        logger.exception(e)
+        await message.reply_text('Some error occurred! Try again later.', quote=True)
+        return
 
 
-@run_async
-def connect_chat(bot, update, args):
-    chat = update.effective_chat  # type: Optional[Chat]
-    user = update.effective_user  # type: Optional[User]
-    if update.effective_chat.type == 'private':
-        if len(args) >= 1:
-            try:
-                connect_chat = int(args[0])
-            except ValueError:
-                update.effective_message.reply_text("Invalid Chat ID provided!")
-            if (bot.get_chat_member(connect_chat, update.effective_message.from_user.id).status in ('administrator', 'creator') or 
-                                     (sql.allow_connect_to_chat(connect_chat) == True) and 
-                                     bot.get_chat_member(connect_chat, update.effective_message.from_user.id).status in ('member')) or (
-                                     user.id in SUDO_USERS):
+@Client.on_message((filters.private | filters.group) & filters.command('disconnect'))
+async def deleteconnection(client, message):
+    userid = message.from_user.id if message.from_user else None
+    if not userid:
+        return await message.reply(f"You are anonymous admin. Use /connect {message.chat.id} in PM")
+    chat_type = message.chat.type
 
-                connection_status = sql.connect(update.effective_message.from_user.id, connect_chat)
-                if connection_status:
-                    chat_name = dispatcher.bot.getChat(connected(bot, update, chat, user.id, need_admin=False)).title
-                    update.effective_message.reply_text("Successfully connected to *{}*".format(chat_name), parse_mode=ParseMode.MARKDOWN)
+    if chat_type == enums.ChatType.PRIVATE:
+        await message.reply_text("Run /connections to view or disconnect from groups!", quote=True)
 
-                    #Add chat to connection history
-                    history = sql.get_history(user.id)
-                    if history:
-                        #Vars
-                        if history.chat_id1:
-                            history1 = int(history.chat_id1)
-                        if history.chat_id2:
-                            history2 = int(history.chat_id2)
-                        if history.chat_id3:
-                            history3 = int(history.chat_id3)
-                        if history.updated:
-                            number = history.updated
+    elif chat_type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
+        group_id = message.chat.id
 
-                        if number == 1 and connect_chat != history2 and connect_chat != history3:
-                            history1 = connect_chat
-                            number = 2
-                        elif number == 2 and connect_chat != history1 and connect_chat != history3:
-                            history2 = connect_chat
-                            number = 3
-                        elif number >= 3 and connect_chat != history2 and connect_chat != history1:
-                            history3 = connect_chat
-                            number = 1
-                        else:
-                            print("Error")
-                    
-                        print(history.updated)
-                        print(number)
+        st = await client.get_chat_member(group_id, userid)
+        if (
+                st.status != enums.ChatMemberStatus.ADMINISTRATOR
+                and st.status != enums.ChatMemberStatus.OWNER
+                and str(userid) not in ADMINS
+        ):
+            return
 
-                        sql.add_history(user.id, history1, history2, history3, number)
-                        print(history.user_id, history.chat_id1, history.chat_id2, history.chat_id3, history.updated)
-                    else:
-                        sql.add_history(user.id, connect_chat, "0", "0", 2)
-                    #Rebuild user's keyboard
-                    keyboard(bot, update)
-                    
-                else:
-                    update.effective_message.reply_text("Connection failed!")
-            else:
-                update.effective_message.reply_text("Connections to this chat not allowed!")
+        delcon = await delete_connection(str(userid), str(group_id))
+        if delcon:
+            await message.reply_text("Successfully disconnected from this chat", quote=True)
         else:
-            update.effective_message.reply_text("Input chat ID to connect!")
-            history = sql.get_history(user.id)
-            print(history.user_id, history.chat_id1, history.chat_id2, history.chat_id3, history.updated)
+            await message.reply_text("This chat isn't connected to me!\nDo /connect to connect.", quote=True)
 
+
+@Client.on_message(filters.private & filters.command(["connections"]))
+async def connections(client, message):
+    userid = message.from_user.id
+
+    groupids = await all_connections(str(userid))
+    if groupids is None:
+        await message.reply_text(
+            "There are no active connections!! Connect to some groups first.",
+            quote=True
+        )
+        return
+    buttons = []
+    for groupid in groupids:
+        try:
+            ttl = await client.get_chat(int(groupid))
+            title = ttl.title
+            active = await if_active(str(userid), str(groupid))
+            act = " - ACTIVE" if active else ""
+            buttons.append(
+                [
+                    InlineKeyboardButton(
+                        text=f"{title}{act}", callback_data=f"groupcb:{groupid}:{act}"
+                    )
+                ]
+            )
+        except:
+            pass
+    if buttons:
+        await message.reply_text(
+            "Your connected group details ;\n\n",
+            reply_markup=InlineKeyboardMarkup(buttons),
+            quote=True
+        )
     else:
-        update.effective_message.reply_text("Usage limited to PMs only!")
-
-
-def disconnect_chat(bot, update):
-    if update.effective_chat.type == 'private':
-        disconnection_status = sql.disconnect(update.effective_message.from_user.id)
-        if disconnection_status:
-            sql.disconnected_chat = update.effective_message.reply_text("Disconnected from chat!")
-            #Rebuild user's keyboard
-            keyboard(bot, update)
-        else:
-           update.effective_message.reply_text("Disconnection unsuccessfull!")
-    else:
-        update.effective_message.reply_text("Usage restricted to PMs only")
-
-
-def connected(bot, update, chat, user_id, need_admin=True):
-    if chat.type == chat.PRIVATE and sql.get_connected_chat(user_id):
-        conn_id = sql.get_connected_chat(user_id).chat_id
-        if (bot.get_chat_member(conn_id, user_id).status in ('administrator', 'creator') or 
-                                     (sql.allow_connect_to_chat(connect_chat) == True) and 
-                                     bot.get_chat_member(user_id, update.effective_message.from_user.id).status in ('member')) or (
-                                     user_id in SUDO_USERS):
-            if need_admin == True:
-                if bot.get_chat_member(conn_id, update.effective_message.from_user.id).status in ('administrator', 'creator') or user_id in SUDO_USERS:
-                    return conn_id
-                else:
-                    update.effective_message.reply_text("You need to be a admin in a connected group!")
-                    exit(1)
-            else:
-                return conn_id
-        else:
-            update.effective_message.reply_text("Group changed rights connection or you are not admin anymore.\nI'll disconnect you.")
-            disconnect_chat(bot, update)
-            exit(1)
-    else:
-        return False
-
-
-
-__help__ = """
-Actions are available with connected groups:
- • View and edit notes
- • View and edit filters
- • More in future!
-
- - /connect <chatid>: Connect to remote chat
- - /disconnect: Disconnect from chat
- - /allowconnect on/yes/off/no: Allow connect users to group
-"""
-
-__mod_name__ = "Connections"
-
-CONNECT_CHAT_HANDLER = CommandHandler("connect", connect_chat, allow_edited=True, pass_args=True)
-DISCONNECT_CHAT_HANDLER = CommandHandler("disconnect", disconnect_chat, allow_edited=True)
-ALLOW_CONNECTIONS_HANDLER = CommandHandler("allowconnect", allow_connections, allow_edited=True, pass_args=True)
-
-dispatcher.add_handler(CONNECT_CHAT_HANDLER)
-dispatcher.add_handler(DISCONNECT_CHAT_HANDLER)
-dispatcher.add_handler(ALLOW_CONNECTIONS_HANDLER)
+        await message.reply_text(
+            "There are no active connections!! Connect to some groups first.",
+            quote=True
+        )
