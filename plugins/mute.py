99cc60b5fb8d9eb2b63bd88d8fb3fcea1e0af5ae
Vishu-993 <116909259+Vishu-993@users.noreply.github.com>
2023-07-07 20:23:12 +0530

    Update mute.py

--- plugins/mute.py
+++ plugins/mute.py
@@ -1,23 +1,49 @@
-from info import,AUTH_USERS,ADMINS
-from pyrogram import filters
-from pyrogram.types import ChatPermissions
+import osimport osimport os
+from telegram import ChatPermissions
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import datetime
 
-
-@app.on_message(filters.command("muteall") & filters.user(ADMINS))
-async def mute_all(_,msg):
-    chat_id=msg.chat.id    
-    bot=await app.get_chat_member(chat_id,AUTH_USERS)
-    bot_permission=bot.privileges.can_restrict_members==True    
-    if bot_permission:
-        async for member in app.get_chat_members(chat_id):       
-            try:
-                    await app.restrict_chat_member(chat_id, member.user.id,ChatPermissions(can_send_messages=False))
-                    await msg.reply_text(f"ᴍᴜᴛɪɴɢ ᴀʟʟ ᴍᴇᴍʙᴇʀs {member.user.mention}")
-                                        
-            except Exception:
-                pass
-    else:
-        await msg.reply_text("ᴇɪᴛʜᴇʀ ɪ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴛʜᴇ ʀɪɢʜᴛ ᴛᴏ ʀᴇsᴛʀɪᴄᴛ ᴜsᴇʀs ᴏʀ ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ɪɴ sᴜᴅᴏ ᴜsᴇʀs")  
-                                         
+# Handler function to process the mute command
+def mute_user(update, context):
+    # Get the user ID of the user to be muted
+    user_id = update.message.reply_to_message.from_user.id
     
-            
+    # Get the mute duration from the command parameter
+    duration_param = context.args[0]
+    duration = parse_duration(duration_param)
+    
+    # Calculate the mute until_date
+    until_date = datetime.datetime.now() + datetime.timedelta(seconds=duration)
+    
+    # Mute the user in the group for the specified duration
+    context.bot.restrict_chat_member(
+        chat_id=update.effective_chat.id,
+        user_id=user_id,
+        permissions=ChatPermissions(can_send_messages=False),
+        until_date=until_date
+    )
+    
+    # Send a confirmation message
+    update.message.reply_text(f"User {user_id} has been muted for {duration_param}.")
+
+# Function to parse the mute duration parameter
+def parse_duration(duration_param):
+    duration_param = duration_param.lower()
+    if duration_param.endswith("h"):
+        hours = int(duration_param[:-1])
+        return hours * 60 * 60
+    elif duration_param.endswith("d"):
+        days = int(duration_param[:-1])
+        return days * 24 * 60 * 60
+    else:
+        raise ValueError("Invalid duration parameter. Please specify the duration in hours (h) or days (d).")
+
+# Get the bot token from environment variable
+bot_token = os.environ.get("BOT_TOKEN")
+
+# Create an instance of the Updater using the bot token
+updater = Updater(token=bot_token, use_context=True)
+
+# Register the mute command handler
+updater.dispatcher.add_handler(CommandHandler('mute', mute_user))
+
