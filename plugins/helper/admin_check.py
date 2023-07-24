8027575c034c3456b42f64037c0908b60c15c6ed
Vishu-993 <116909259+Vishu-993@users.noreply.github.com>
2023-07-25 01:30:46 +0530

    Update admin_check.py

--- plugins/helper/admin_check.py
+++ plugins/helper/admin_check.py
@@ -1,7 +1,7 @@
 from pyrogram.types import Message
-from pyrogram import Client, filters, enums
+from pyrogram import filters, enums 
 
-# Helper function to check for admin and owner privileges
+
 async def admin_check(message: Message) -> bool:
     if not message.from_user:
         return False
@@ -23,13 +23,14 @@ async def admin_check(message: Message) -> bool:
         chat_id=chat_id,
         user_id=user_id
     )
-    
-    admin_strings = [
-        enums.ChatMemberStatus.CREATOR,
-        enums.ChatMemberStatus.ADMINISTRATOR,
-    ]
-    
+    admin_strings = [enums.ChatMemberStatus.OWNER, enums.ChatMemberStatus.ADMINISTRATOR]
+    # https://git.colinshark.de/PyroBot/PyroBot/src/branch/master/pyrobot/modules/admin.py#L69
     if check_status.status not in admin_strings:
         return False
     else:
         return True
+
+async def admin_filter_f(filt, client, message):
+    return await admin_check(message)
+
+admin_fliter = filters.create(func=admin_filter_f, name="AdminFilter")
