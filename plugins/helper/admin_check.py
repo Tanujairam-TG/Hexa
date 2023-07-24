cb66efd5f61e104a8e624e056cabfe57cf94d25f
Vishu-993 <116909259+Vishu-993@users.noreply.github.com>
2023-07-25 01:11:49 +0530

    Update admin_check.py

--- plugins/helper/admin_check.py
+++ plugins/helper/admin_check.py
@@ -1,6 +1,5 @@
-from pyrogram.types import Message
-from pyrogram import filters, enums 
-
+from pyrogram.types import Message, ChatMember
+from pyrogram import filters, enums
 
 async def admin_check(message: Message) -> bool:
     if not message.from_user:
@@ -19,18 +18,11 @@ async def admin_check(message: Message) -> bool:
     chat_id = message.chat.id
     user_id = message.from_user.id
 
-    check_status = await client.get_chat_member(
+    # Get information about the chat member (user)
+    chat_member = await client.get_chat_member(
         chat_id=chat_id,
         user_id=user_id
     )
-    admin_strings = [enums.ChatMemberStatus.OWNER, enums.ChatMemberStatus.ADMINISTRATOR]
-    # https://git.colinshark.de/PyroBot/PyroBot/src/branch/master/pyrobot/modules/admin.py#L69
-    if check_status.status not in admin_strings:
-        return False
-    else:
-        return True
 
-async def admin_filter_f(filt, client, message):
-    return await admin_check(message)
-
-admin_fliter = filters.create(func=admin_filter_f, name="AdminFilter")
+    # Check if the user is either the owner or an administrator of the chat
+    return chat_member.status in [ChatMember.OWNER, ChatMember.ADMINISTRATOR]
