5df30b13ad133c05155a93420054ab6a1f690683
Vishu-993 <116909259+Vishu-993@users.noreply.github.com>
2023-07-30 16:26:08 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -478,6 +478,16 @@ async def settings(client, message):
                     '✅ Yes' if settings["welcome"] else '❌ No',
                     callback_data=f'setgs#welcome#{settings["welcome"]}#{grp_id}',
                 ),
+            ],
+            [
+                InlineKeyboardButton(
+                    'Goodbye',
+                    callback_data=f'setgs#goodbye#{settings["goodbye"]}#{grp_id}',
+                ),
+                InlineKeyboardButton(
+                    '✅ Yes' if settings["goodbye"] else '❌ No',
+                    callback_data=f'setgs#goodbye#{settings["goodbye"]}#{grp_id}',
+                ),
             ]
         ]
         else:
@@ -540,6 +550,16 @@ async def settings(client, message):
                 InlineKeyboardButton(
                     '✅ Yes' if settings["welcome"] else '❌ No',
                     callback_data=f'setgs#welcome#{settings["welcome"]}#{grp_id}',
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    'Goodbye',
+                    callback_data=f'setgs#goodbye#{settings["goodbye"]}#{grp_id}',
+                ),
+                InlineKeyboardButton(
+                    '✅ Yes' if settings["goodbye"] else '❌ No',
+                    callback_data=f'setgs#goodbye#{settings["goodbye"]}#{grp_id}',
                 ),
             ]
         ]
