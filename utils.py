d20ab9eebade636ed8a62e13d6c37a736b41584d
Vishu-993 <116909259+Vishu-993@users.noreply.github.com>
2023-08-08 23:57:01 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -157,9 +157,9 @@ async def broadcast_messages(user_id, message):
 
 async def search_gagala(text):
     usr_agent = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'
-}
-
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+        'Chrome/61.0.3163.100 Safari/537.36'
+    }
     text = text.replace(" ", '+')
     url = f'https://www.google.com/search?q={text}'
     response = requests.get(url, headers=usr_agent)
